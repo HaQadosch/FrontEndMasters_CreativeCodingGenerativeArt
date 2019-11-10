a3d504f5474e03aa8c39cd2ed9bbd2afa153d2e6
HaQadosch Berraka <haqadosch@pm.me>
2019-11-10 15:21:15 +0000

    Drawing the Grid

--- genart/sketch.js
+++ genart/sketch.js
@@ -9,19 +9,39 @@ const settings = {
 }
 
 const sketch = () => {
+  // Local state functions.
+  const createGrid = () => {
+    const points = []
+    const count = 5 // Grid size, meaning 5x5.
+    for (let x = 0; x < count; x++) {
+      for (let y = 0; y < count; y++) {
+        // sets the space (width & height) between 0 and 1.
+        // Normalize?
+        const u = count <= 1 ? 0.5 : x / (count - 1)
+        const v = count <= 1 ? 0.5 : y / (count - 1)
+        points.push([ u, v ])
+      }
+    }
+    return points
+  }
+
+  const points = createGrid()
+  // Render function.
   return ({ context, width, height }) => {
-    context.fillStyle = 'orange'
+    context.fillStyle = 'white'
     context.fillRect(0, 0, width, height)
 
-    // Rendering a circle is rendering a path.
-    context.beginPath()
-    context.fillStyle = 'purple'
-    context.arc(width / 2, height / 2, width * 0.2, 0, 2 * Math.PI, false)
-    context.fill()
+    points.forEach(([ u, v ]) => {
+      // Project back to the 2048 pixels dimension.
+      const x = u * width
+      const y = v * height
 
-    context.lineWidth = width * 0.05
-    context.strokeStyle = 'green'
-    context.stroke()
+      context.beginPath()
+      context.arc(x, y, 100, 0, Math.PI * 2, false)
+      context.strokeStyle = 'black'
+      context.lineWidth = 40
+      context.stroke()
+    })
   }
 }
 
